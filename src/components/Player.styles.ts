50cfbf92b2ace49721837e6a7bfff9ac8b371bb2
Chris Allmark <chris.allmark@gmail.com>
2023-03-21 20:43:58 +0000

    Fix colours

--- src/components/Player.styles.ts
+++ src/components/Player.styles.ts
@@ -4,14 +4,20 @@ export const Dropdown = styled.div`
   .ui {
     .menu {
       background-color: #1b1c1d;
+      > .message:not(.ui) {
+        color: red;
+      }
       .text {
         color: white;
       }
+    }
+    .search {
+      color: white;
     }
   }
 `;
 
 export const MadWorldLogo = styled.img`
   max-width: 1366px;
-  width: 100%;
+  width: 10 0%;
 `;
