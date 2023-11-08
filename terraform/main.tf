a49dcea7cb1aafc7e73840d997bc406efdaa447b
Chris Allmark <chris.allmark@gmail.com>
2023-11-08 20:22:08 +0000

    Add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "madworld" {
+  bucket = var.bucket
+}
+
+resource "aws_s3_bucket_acl" "madworld" {
+  bucket     = aws_s3_bucket.madworld.id
+  acl        = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.madworld]
+}
+
+resource "aws_s3_bucket_ownership_controls" "madworld" {
+  bucket = aws_s3_bucket.madworld.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "madworld" {
+  bucket              = aws_s3_bucket.madworld.id
+  block_public_acls   = false
+  block_public_policy = false
+}
+
+resource "aws_s3_bucket_cors_configuration" "madworld" {
+  bucket = aws_s3_bucket.madworld.id
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["https://mad-world.vercel.app"]
+    expose_headers = [
+      "Content-Length",
+      "Content-Typeid",
+      "ETag"
+    ]
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_object" "madworld-extras" {
+  for_each = fileset("../public/extras/", "**")
+  acl      = "public-read"
+  bucket   = var.bucket
+  etag     = filemd5("../public/extras/${each.value}")
+  key      = "extras/${each.value}"
+  source   = "../public/extras/${each.value}"
+}
+
+resource "aws_s3_object" "madworld-samples" {
+  for_each = fileset("../public/samples/", "**")
+  acl      = "public-read"
+  bucket   = var.bucket
+  etag     = filemd5("../public/samples/${each.value}")
+  key      = "samples/${each.value}"
+  source   = "../public/samples/${each.value}"
+}
+
+resource "aws_s3_object" "madworld-tracks" {
+  for_each = fileset("../public/tracks/", "**")
+  acl      = "public-read"
+  bucket   = var.bucket
+  etag     = filemd5("../public/tracks/${each.value}")
+  key      = "tracks/${each.value}"
+  source   = "../public/tracks/${each.value}"
+}
